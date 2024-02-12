8306913689789fb4fb102e0a7875f6e661cc78bf
justthatonedude <jothamgao@gmail.com>
2024-02-12 10:12:31 -0500

    4

--- vue-project/src/components/ItemCard.vue
+++ vue-project/src/components/ItemCard.vue
@@ -8,7 +8,7 @@
         <p>{{ item.storage }} Storage</p>
         <p>{{ item.operating_system }}</p>
         <p>${{ item.price }}</p>
-        <button class="btn">Add To Cart</button>
+        <button class="btn" @click="AddCart">Add To Cart</button>
     </div>
 </template>
 
@@ -16,6 +16,14 @@
 const props = defineProps({
     item: Object
 })
+import {ref} from "vue";
+let clicked = ref(0);
+const productContainer = document.querySelector(".productCard")
+function AddCart(){
+    productContainer.insertAdjacentHTML(
+        'afterend','<div class="cart"><h2>Cart</h2></div>'
+    )
+}
 </script>
 
 <style scoped>
@@ -36,10 +44,10 @@ const props = defineProps({
     letter-spacing: .0892857143em;
     text-decoration: none;
     text-transform: uppercase;
-    min-width: 64px;
+    min-width: 60px;
     border: 1px solid #6200ee;
     text-align: center;
-    transition: all 0.3s;
+    transition: all 0.5s;
                 
 }
 img{
@@ -48,12 +56,11 @@ img{
     object-fit: cover;
 }
 .productCard{
+    border-radius: px;
+    border: 4px solid #000000 ;
     margin: 10px;
-    height: 549px;
-    width: 305px;
 }
 .btn:hover {
-    transform: scale(1.1);
     background-color: #6200ee;
     color: #FFFFFF;
     };
